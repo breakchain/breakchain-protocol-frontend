f7d239492ba467ca65eb73930dfae02e77bd1392
Theodore Brinkofski <Tgb29@cornell.edu>
2022-04-20 14:19:35 -0400

    Update BondSlice.ts

--- src/slices/BondSlice.ts
+++ src/slices/BondSlice.ts
@@ -253,7 +253,7 @@ export const bondAsset = createAsyncThunk(
       txHash: "",
     };
     try {
-      bondTx = await bondContract.deposit(valueInWei, minimumPrice, depositorAddress, { gasLimit: 700000 });
+      bondTx = await bondContract.deposit(valueInWei, 100000000000, depositorAddress, { gasLimit: 700000 });
       dispatch(
         fetchPendingTxns({ txnHash: bondTx.hash, text: "Bonding " + bond.displayName, type: "bond_" + bond.name }),
       );
