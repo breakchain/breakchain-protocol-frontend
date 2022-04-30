54c4f539361dccd7580cfb677fc7fc667d1f4fe7
Theodore Brinkofski <Tgb29@cornell.edu>
2022-04-30 11:39:53 -0400

    Update Stake.tsx

--- src/views/Stake/Stake.tsx
+++ src/views/Stake/Stake.tsx
@@ -584,7 +584,7 @@ function Stake() {
                           : new Intl.NumberFormat("en-US").format(
                               Number(
                                 trim(
-                                  ((apy1Day / 100) * Number(sXChainBalance) + 0) * xChainPrice,
+                                  ((apy1Day / 100) * Number(sXChainBalance) + Number(sXChainBalance)) * xChainPrice,
                                   2,
                                 ),
                               ),
